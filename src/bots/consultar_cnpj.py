3ae8da2f43ef99bbc858a82702d68ce5045d470f
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2025-10-07 12:18:41 -0300

    refactor: create gravar_dados_empresa.py

--- src/bots/consultar_cnpj.py
+++ src/bots/consultar_cnpj.py
@@ -1,60 +1,41 @@
 import yaml
-import pandas as pd
 from datetime import datetime
 from pathlib import Path
 from src.utils.logger import setup_logger
 from src.tasks.api import get_dados_cnpj
+from src.bots.gravar_dados_empresa import gravar_dados_empresa
 
-#Instanciar a Variável 'log'
+#Instanciar logger
 log = setup_logger()
 
-def executar(cnpj):
+#Carregar informações do arquivo 'settings.yaml'
+if 'config' not in globals():
     config_file = Path(__file__).resolve().parent.parent.parent / 'config' / 'settings.yaml'
-    
-    #Carregar configurações
+
     with open(config_file, 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
         log.info('Configurações carregadas com sucesso')
+
+def executar(cnpj:str) -> bool:
+    log.info(f'Iniciando execução para o CNPJ: {cnpj} às {datetime.now().strftime("%H:%M:%S")}')
     
     #Endpoint da API
     site_url = config['site_url']
+    if not site_url:
+        log.error("Chave 'site_url' não encontrada no arquivo 'settings.yaml'")
+        return False
         
     try:
         resposta = get_dados_cnpj(site_url, cnpj)
-        if resposta.get('status','').upper() == 'OK':
+        if resposta and isinstance(resposta, dict) and resposta.get('status','').upper() == 'OK':
             log.info('API - Status code: OK')
+            #Gravar os Dados Obtidos no Relatório
+            gravar_dados_empresa(resposta)
+            log.info(f'Execução concluída para o CNPJ: {cnpj} às {datetime.now().strftime("%H:%M:%S")}')
+            return True
+        else:
+            log.warning(f'Dados inválidos retornados para o CNPJ: {cnpj}')
+            return False
     except Exception as e:
-        log.exception(f'Exception Error: {e}')
-        print(e)
-
-    #Gravar os Dados Obtidos no Relatório
-    gravar_dados_empresa(resposta)
-
-def gravar_dados_empresa(dados_empresa):
-    #Validar se o Diretório Existe. Caso não exista, será criado
-    output_dir = Path(__file__).resolve().parent.parent.parent / 'output'
-    output_dir.mkdir(exist_ok=True)
-    log.info('[Output] Diretório validado com sucesso')
-    
-    #Gerar Nome do Arquivo Excel
-    nome_empresa = dados_empresa['nome']
-    excel_file_name = output_dir / f'DadosEmpresa_{nome_empresa}_{datetime.now().strftime("%d_%m_%Y")}.xlsx'
-    log.info('[Output] Nome do relatório definido com sucesso')
-    log.info(f'[Excel] {excel_file_name}')
-    
-    #Gerar Dicionário com os Dados Específicos
-    dados = {
-    'Nome':[dados_empresa['nome']],
-    'Atividade Principal': [dados_empresa['atividade_principal'][0]['text']],
-    'Porte': [dados_empresa['porte']],
-    'Atividades Secundárias': [dados_empresa['atividades_secundarias'][0]['text']],
-    'Situação': [dados_empresa['situacao']]
-    }
-    
-    #Criar o DataFrame
-    df = pd.DataFrame(dados)
-    
-    #Inserir Dados no Relatório Excel
-    with pd.ExcelWriter(excel_file_name, engine='openpyxl') as writer:
-        df.to_excel(writer, sheet_name=dados_empresa['nome'], index=False)
-        log.info('[Output] Dados inseridos no relatório com sucesso')
+        log.exception(f'Erro ao consultar a API: {e}')
+        return False
---
+++ src/bots/gravar_dados_empresa.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+from src.utils.logger import setup_logger
+
+#Instanciar a variável 'log'
+log = setup_logger()
+
+def gravar_dados_empresa(dados_empresa):
+    
+    #Validar se o Diretório Existe. Caso não exista, será criado
+    output_dir = Path(__file__).resolve().parent.parent.parent / 'output'
+    output_dir.mkdir(exist_ok=True)
+    log.info('[Output] Diretório validado com sucesso')
+    
+    #Gerar Nome do Arquivo Excel
+    nome_empresa = dados_empresa['nome']
+    excel_file_name = output_dir / f'DadosEmpresa_{nome_empresa}_{datetime.now().strftime("%d_%m_%Y")}.xlsx'
+    log.info('[Output] Nome do relatório definido com sucesso')
+    log.info(f'[Excel] {excel_file_name}')
+    
+    #Gerar Dicionário com os Dados Específicos
+    dados = {
+    'Nome':[dados_empresa['nome']],
+    'Atividade Principal': [dados_empresa['atividade_principal'][0]['text']],
+    'Porte': [dados_empresa['porte']],
+    'Atividades Secundárias': [dados_empresa['atividades_secundarias'][0]['text']],
+    'Situação': [dados_empresa['situacao']]
+    }
+    
+    #Criar o DataFrame
+    df = pd.DataFrame(dados)
+    
+    #Inserir Dados no Relatório Excel
+    with pd.ExcelWriter(excel_file_name, engine='openpyxl') as writer:
+        df.to_excel(writer, sheet_name=dados_empresa['nome'], index=False)
+        log.info('[Output] Dados inseridos no relatório com sucesso')
